77b03f7d8c2693d44a610139b57f5f9352021163
timyorku <tim17@my.yorku.ca>
2024-11-01 22:03:59 -0400

    Delete app directory

--- app/bargain_box/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home_page(request):
-    context = {
-        'page_title': 'Home'
-    }
-    return render(request, 'home/home.html', context)
